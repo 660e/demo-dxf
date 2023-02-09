d441614233967456f1913d37d3cd120b40adbf86
Min <solunar@vip.qq.com>
2023-02-09 14:36:15 +0800

    Update

--- pages/dxf.js
+++ pages/dxf.js
@@ -1,1 +1,21 @@
-console.log(dxf);
+const file = document.querySelector('#file');
+const section = document.querySelector('#section');
+
+section.onclick = () => file.click();
+
+file.addEventListener('change', event => {
+  const reader = new FileReader();
+
+  reader.readAsBinaryString(event.target.files[0]);
+  reader.onload = e => {
+    if (e.target.readyState === 2) {
+      const helper = new dxf.Helper(e.target.result);
+
+      section.innerHTML = helper.toSVG();
+
+      console.log(helper._parsed);
+    } else {
+      console.log('dxf文件解析失败');
+    }
+  };
+});
